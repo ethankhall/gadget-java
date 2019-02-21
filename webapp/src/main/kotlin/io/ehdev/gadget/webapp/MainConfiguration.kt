b6db4ed33f3b1e7cbbb3350c81b140fe5bb47f2f
Ethan Hall <ethankhall@pm.me>
2019-02-21 11:33:41 -0800

    GraphiteMeterRegistry will report with a prefix

--- webapp/src/main/kotlin/io/ehdev/gadget/webapp/MainConfiguration.kt
+++ webapp/src/main/kotlin/io/ehdev/gadget/webapp/MainConfiguration.kt
@@ -1,10 +1,25 @@
 package io.ehdev.gadget.webapp
 
+import io.micrometer.core.instrument.Clock
+import io.micrometer.core.instrument.util.HierarchicalNameMapper
+import io.micrometer.graphite.GraphiteConfig
+import io.micrometer.graphite.GraphiteMeterRegistry
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.EnableAspectJAutoProxy
+import org.springframework.core.env.Environment
 
 @Configuration
 @SpringBootApplication
 @EnableAspectJAutoProxy
-open class MainConfiguration
+open class MainConfiguration {
+    @Bean
+    open fun graphiteMeterRegistry(config: GraphiteConfig, clock: Clock, env: Environment): GraphiteMeterRegistry {
+        val prefix = env.getProperty("metric.prefix", "gadget")
+
+        return GraphiteMeterRegistry(
+                config, clock,
+                HierarchicalNameMapper { id, convention -> "$prefix." + HierarchicalNameMapper.DEFAULT.toHierarchicalName(id, convention) })
+    }
+}
