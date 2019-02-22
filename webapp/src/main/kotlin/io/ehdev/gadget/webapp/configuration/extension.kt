45e452052609a8ceebefea163c251078bbdd4061
Ethan Hall <ethankhall@pm.me>
2019-02-22 11:27:39 -0800

    Use firstOrNull

--- webapp/src/main/kotlin/io/ehdev/gadget/webapp/configuration/extension.kt
+++ webapp/src/main/kotlin/io/ehdev/gadget/webapp/configuration/extension.kt
@@ -3,5 +3,5 @@ package io.ehdev.gadget.webapp.configuration
 import org.springframework.web.reactive.function.server.ServerRequest
 
 fun ServerRequest.findScheme(): String {
-    return this.headers().header("X-Forwarded-Proto").first() ?: this.uri().scheme
+    return this.headers().header("X-Forwarded-Proto").firstOrNull() ?: this.uri().scheme
 }
